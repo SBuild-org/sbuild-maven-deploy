cdf59eab41bb65558cf452068b94add5c9a12fb4
Tobias Roeser <le.petit.fou@web.de>
2013-12-21 17:19:18 +0100

    Adapted to new plugin API.

--- org.sbuild.plugins.mavendeploy/SBuild.scala
+++ org.sbuild.plugins.mavendeploy/SBuild.scala
@@ -4,20 +4,14 @@ import de.tototec.sbuild.ant.tasks._
 
 import de.tototec.sbuild._
 
-@version("0.7.0")
-@classpath("../../sbuild-plugin/org.sbuild.plugins.sbuildplugin/target/org.sbuild.plugins.sbuildplugin-0.0.9000.jar")
+@version("0.7.1")
+@classpath("mvn:org.sbuild:org.sbuild.plugins.sbuildplugin:0.2.0")
 class SBuild(implicit _project: Project) {
 
-  Plugin[org.sbuild.plugins.sbuildplugin.SBuildPlugin] configure { c =>
-    // the version of SBuild this plugin is compatible to
-    c.sbuildVersion = "0.7.0"
-
-    // the plugin API
-    c.pluginClass = "org.sbuild.plugins.mavendeploy.MavenDeploy"
-    // the version of this plugin
-    c.pluginVersion = "0.0.9000"
-    // the plugin factory class, which extends trait de.tototec.sbuild.Plugin
-    c.pluginFactoryClass = "org.sbuild.plugins.mavendeploy.MavenDeployPlugin"
-  }
+  Plugin[org.sbuild.plugins.sbuildplugin.SBuildPlugin] configure { _.copy(
+    sbuildVersion = "0.7.1",
+    pluginClass = "org.sbuild.plugins.mavendeploy.MavenDeploy",
+    pluginVersion = "0.0.9000"
+  )}
 
 }
